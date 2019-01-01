b8f31904cee21dd7807b96bddb579c8817561497
cosiek <witold.majchrzak@21s.pl>
2019-01-01 16:04:23 +0100

    # add periodic callback "hello world"

--- server.py
+++ server.py
@@ -6,14 +6,21 @@ import socket
 import tornado.ioloop
 import tornado.web
 
+
+def run_periodic():
+    print("Jup!")
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+
 
 if __name__ == "__main__":
     port = 8888
@@ -29,5 +36,9 @@ if __name__ == "__main__":
     # get loop
     loop = tornado.ioloop.IOLoop.current()
     
+    # schedule tasks
+    pc = tornado.ioloop.PeriodicCallback(run_periodic, 5000)
+    pc.start()
+
     # start server
     loop.start()
